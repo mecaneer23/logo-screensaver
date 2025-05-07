7845774c7ab73dc47aa73a4e41ea2fbbeaff71ef
mecaneer23 <mecaneer23@gmail.com>
2025-05-07 20:08:48 +0300

    feat: change to random color on each bounce

--- README.md
+++ README.md
@@ -4,5 +4,4 @@ Animate a logo bouncing around a screen
 
 ## Todo
 
-- feat: change to random color on each bounce
 - feat: allow for quitting by pressing any key, not just ctrl+c
--- bounce.py
+++ bounce.py
@@ -5,12 +5,35 @@ Animate a logo bouncing around a screen
 
 import curses
 import sys
+from enum import Enum
 from pathlib import Path
+from random import choice
 
+
+class Color(Enum):
+    RED = 1
+    GREEN = 2
+    YELLOW = 3
+    BLUE = 4
+    MAGENTA = 5
+    CYAN = 6
+    WHITE = 7
+
+    def as_int(self) -> int:
+        """
+        Returns the color pair value corresponding to the color
+        """
+        return self.value
+
+    @staticmethod
+    def values() -> list[int]:
+        """
+        Returns a list of all color values
+        """
+        return [color.value for color in Color]
 
 class BouncingLogo:
     def __init__(self, filename: Path, stdscr: curses.window) -> None:
-        curses.curs_set(0)
         self._stdscr = stdscr
         with filename.open("r") as file:
             self._text = file.read().splitlines()
@@ -20,17 +43,21 @@ class BouncingLogo:
         self._y = 0
         self._x_vel = 2
         self._y_vel = 1
+        self._color = curses.color_pair(Color.WHITE.as_int())
 
     def _display(self, y: int, x: int) -> None:
         self._stdscr.clear()
         for row_index, line in enumerate(self._text):
-            self._stdscr.addstr(y + row_index, x, line)
+            self._stdscr.addstr(y + row_index, x, line, self._color)
         self._stdscr.refresh()
 
     def _move(self) -> None:
         self._check_collision()
         self._x += self._x_vel
         self._y += self._y_vel
+
+    def _update_color(self, color: int) -> None:
+        self._color = curses.color_pair(color)
 
     def _check_collision(self) -> None:
         lines, cols = self._stdscr.getmaxyx()
@@ -39,10 +66,12 @@ class BouncingLogo:
             self._x + self._width >= cols - 1 and self._x_vel > 0
         ):
             self._x_vel *= -1
+            self._update_color(choice(Color.values()))  # noqa: S311
         if (self._y <= 0 and self._y_vel < 0) or (
             self._y >= lines - self._height and self._y_vel > 0
         ):
             self._y_vel *= -1
+            self._update_color(choice(Color.values()))  # noqa: S311
 
     def start(self) -> None:
         while True:
@@ -51,7 +80,26 @@ class BouncingLogo:
             curses.napms(100)
 
 
+def _init() -> None:
+    curses.use_default_colors()
+    curses.curs_set(0)
+    for i, color in enumerate(
+        (
+            curses.COLOR_RED,
+            curses.COLOR_GREEN,
+            curses.COLOR_YELLOW,
+            curses.COLOR_BLUE,
+            curses.COLOR_MAGENTA,
+            curses.COLOR_CYAN,
+            curses.COLOR_WHITE,
+        ),
+        start=1,
+    ):
+        curses.init_pair(i, color, -1)
+
+
 def main(stdscr: curses.window) -> int:
+    _init()
     animation = BouncingLogo(Path("logo.txt"), stdscr)
     try:
         animation.start()
